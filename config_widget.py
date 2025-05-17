0d49f38042826fd4d652003eacda2f817327187d
Aucismy <89524851158@mail.ru>
2025-05-17 10:55:40 +0300

    The file with the interface for the main program window

---
+++ config_widget.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from tkinter import ttk
+
+def configurate_grid(win:Tk|Frame, row_count:int, column_count:int):
+    for r in range(row_count): win.rowconfigure(index=r, weight=1)
+    for c in range(column_count): win.columnconfigure(index=c, weight=1)
+
+def configurate_table(win: Tk|Frame, dict_columns):
+    table = ttk.Treeview(win, columns=list(dict_columns.keys()), show='headings')
+    for k, v in dict_columns.items():
+        table.heading(k, text=v[0])
+        table.column(k, minwidth=v[1], width=v[1], stretch=v[2])
+
+    return table
