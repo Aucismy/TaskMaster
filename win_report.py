711b52c38c6c7f65e33060857e298416d2bf689d
Aucismy <89524851158@mail.ru>
2025-05-29 08:10:06 +0300

    Изиенение окна отчётов

--- win_report.py
+++ win_report.py
@@ -5,7 +5,7 @@ from config_widget import configurate_grid
 from tkcalendar import DateEntry
 
 def widgets():
-    configurate_grid(win, 10, 4)
+    configurate_grid(win, 7, 4)
 
     label_rep = Label(win, text='Отчёт')
     label_rep.grid(row=0, column=1, columnspan=2, sticky=EW)
@@ -30,9 +30,21 @@ def widgets():
     type_report = Combobox(win, values=['По проектам', 'По приоритетам'])
     type_report.grid(row=3, column=2, sticky=EW)
 
+    canvas_grap = Canvas(win, background='red', height=1)
+    canvas_grap.grid(row=4, column=0, columnspan=2, padx=2.5, pady=5, sticky=NSEW)
+
+    canvas_diag = Canvas(win, background='blue')
+    canvas_diag.grid(row=4, column=2, columnspan=2, padx=2.5, pady=5, sticky=NSEW)
+
+    button_export = Button(win, text='Экспорт')
+    button_export.grid(row=5, column=2, sticky=E, padx=5)
+
+    combo_export = Combobox(win, values=['PDF', 'CSV'], width=10)
+    combo_export.grid(row=5, column=3, sticky=W, padx=5)
+
 win = Tk()
 win.title('TaskMaster')
-win.geometry('400x500+650+300')
+win.geometry('600x500+650+300')
 
 widgets()
 
